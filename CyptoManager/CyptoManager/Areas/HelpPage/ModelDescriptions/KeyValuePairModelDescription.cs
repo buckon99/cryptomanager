d66eb85256e16f9948948495d7fca84d23937f47
Jack Buckon <jack.techark@gmail.com>
2017-12-22 16:00:53 -0800

    removed old ASP.NET project

--- CyptoManager/CyptoManager/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace CyptoManager.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
