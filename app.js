bbc7e420c09b2cfabbb07085d58c2a03a04cf012
Jack Buckon <jack.techark@gmail.com>
2017-12-22 16:19:39 -0800

    prepared project for nodejs development

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+var ejs = require('ejs');
+const chalk = require('chalk');
+
+const homeController = require ('./controllers/home');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'production'));
+
+app.use('/', express.static('.'));
+app.get('/index', homeController.index);
+app.listen(app.get('port'), () => {
+    console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env')); 
+    console.log('  Press CTRL-C to stop\n');
+  });
+  
